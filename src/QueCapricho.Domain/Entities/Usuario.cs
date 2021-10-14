a7fbea96d8cc92a45002522691461f1e51fbd75a
Guilherme Lopes <gui-lopespereira@hotmail.com>
2021-10-13 23:09:22 -0300

    Alteração da tabela Usuario

--- src/QueCapricho.Domain/Entities/Usuario.cs
+++ src/QueCapricho.Domain/Entities/Usuario.cs
@@ -9,12 +9,6 @@ namespace QueCapricho.Domain.Entities
         public string CPF { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-        public string Endereco { get; set; }
-        public string Telefone { get; set; }
-        public DateTime DataNascimento { get; set; }
-        public DateTime DataCadastro { get; set; }
-        public decimal RendaMensal { get; set; }
-        public decimal SaldoConta { get; set; }
         public bool Apagado { get; set; }
     }
 }
